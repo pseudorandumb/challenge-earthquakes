30ffafc7a359340eaaf1119c8d8dcced7d86fef7
Kyle Patterson <toodlestech@gmail.com>
2016-02-21 21:31:59 -0800

    Display error and info if no earthquakes are found

--- most-dangerous-places.rb
+++ most-dangerous-places.rb
@@ -2,10 +2,14 @@ require './environment'
 
 def mostDangerousPlaces(num_places, num_days)
   earthquakes = Earthquake.dangerous_places(num_places, num_days)
-  adjust_spacing = 40
-  puts "PLACE".ljust(adjust_spacing+1) + "EQ-MAGNITUDE"
-  earthquakes.each do |earthquake|
-    puts "#{earthquake.place.ljust(adjust_spacing)} #{earthquake.magnitude}"
+  if earthquakes.empty?
+    puts "No earthquakes found, please run 'rake earthquakes:update_local'"
+  else
+    adjust_spacing = 40
+    puts "PLACE".ljust(adjust_spacing+1) + "EQ-MAGNITUDE"
+    earthquakes.each do |earthquake|
+      puts "#{earthquake.place.ljust(adjust_spacing)} #{earthquake.magnitude}"
+    end
   end
 end
 
